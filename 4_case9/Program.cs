b1d4c94020ab03a3ee167ddd51e6888f8c596d88
Warren <warrenbao@outlook.com>
2022-04-17 21:43:39 +0800

    case 99乘法表

---
+++ 4_case9/Program.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace _4_case9
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            for(int i= 1; i<=9 ; i++)
+            {
+                for (int j=1; j<=i; j++)
+                {
+                    Console.Write("{0}*{1}={2} ", j, i, j*i);
+                }
+                Console.WriteLine();
+            }
+            Console.ReadLine();
+        }
+    }
+}
