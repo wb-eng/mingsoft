286e495a1f5b242fc7e41629c7e5ea050482eebb
Warren <warrenbao@outlook.com>
2022-04-18 20:21:23 +0800

    一维数组

---
+++ array_1d/Program.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace array_1d
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            int[] day = new int[] { 31, 28, 31, 30, 31, 30, 31, 31, 30, 31, 30, 31 };
+            for(int i= 0; i<12; i++)
+            {
+                Console.WriteLine("{0} yue you {1} tian",i , day[i]);
+            }
+            Console.ReadLine();
+        }
+    }
+}
